f825bcebc13196fc068d6645c4312fd1de7d6152
renatoirina <irinarenato793@gmail.com>
2024-05-03 16:01:55 +0200

    feat: operation test

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import HeaderPage from "./components/HeaderPage.vue";
+
+  export default {
+    components: {
+      HeaderPage,
+    },
+    data () {
+      return {};
+    },
+  };
+</script>
+
+<template>
+  <HeaderPage></HeaderPage>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+    data () {
+      return {
+        test: "prova",
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{test}}</h1>
+</template>
+
+<style>
+
+</style>
